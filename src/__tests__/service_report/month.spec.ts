1a8c6ddb26ff14511395cda0b0c37db3315087d4
Iverson <ivcotad@gmail.com>
2024-11-27 18:38:21 -0700

    fixup! migrate to modual first structure

--- src/__tests__/service_report/month.spec.ts
+++ src/__tests__/service_report/month.spec.ts
@@ -4,9 +4,9 @@ import { Month } from "@/modual/service_report/domain/value_objects/month";
 
 describe("Month", () => {
   it("should create a month object with a range based on a date within the month", () => {
-    const month = new Month(new Date("2024-01-15"));
-    const monthStart = new Date("2024-01-01");
-    const monthEnd = new Date("2024-01-31");
+    const month = new Month(new Date(Date.UTC(2024, 0, 15)));
+    const monthStart = new Date(Date.UTC(2024, 0, 1));
+    const monthEnd = new Date(Date.UTC(2024, 1, 1));
 
     expect(month.monthStart).toEqual(monthStart);
     expect(month.monthEnd).toEqual(monthEnd);
